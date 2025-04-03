1f8eb6a6a8ce1349b964dac2862dde41d33933dd
janidu22 <janidudhakshi222@gmail.com>
2025-04-04 02:55:43 +0530

    interface done

---
+++ StudentManagementAPI/IStudents.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementAPI
+{
+    public interface IStudents
+    {
+        int id { get; set; }
+        string Firstname { get; set; }
+        string Lastname { get; set; }
+        decimal DegreeFee { get; set; }
+
+    }
+}
